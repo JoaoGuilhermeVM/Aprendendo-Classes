6a0be27090ebe9bd97e9888d217b29ea00b1f2b2
João Guilherme V. Moitinho <106176186+JoaoGuilhermeVM@users.noreply.github.com>
2026-06-21 13:15:35 -0300

    Adicionar arquivos de projeto.

---
+++ Aprendendo Classes/Program.cs
@@ -0,0 +1,11 @@
+﻿Random dice = new Random();
+int roll0 = dice.Next(int.Parse(Console.ReadLine()));//gera um numero aleatorio entre 0 e o valor digitado pelo usuario.
+int roll1 = dice.Next();//gera um numero aleatorio entre 0 e o valor maximo de um inteiro.
+int roll2 = dice.Next(101);//gera um numero aleatorio entre 0 e 100.
+int roll3 = dice.Next(50, 101);//gera um numero aleatorio entre 50 e 100.
+
+Console.WriteLine($"First roll: {roll0}");
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+//entendendo o que é uma classe, metodo e objeto.
